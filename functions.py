9bf3e27663075516cfc2d1f1bc7c84366761d2e8
Paun Darius <paundariualexandru@gmail.com>
2024-01-07 19:07:44 +0200

    First zone

--- functions.py
+++ functions.py
@@ -9,7 +9,7 @@ def getWords(leng):
     new_list = [word.lower() for word in old_list if len(word) == leng]
     return new_list
 def checkWord(word) -> bool:
-    if(len(list(word))!=5):
+    if len(word) != 5:
         print("YOUR WORD MUST HAVE 5 LETTERS")
         return False
     Letters=[let for let in word]
@@ -19,12 +19,6 @@ def checkWord(word) -> bool:
             print(f"{let} IS NOT ALLOWED")
             return False
     return True
-def processWord(word):
-    #word = get_random_word()
-    randWord = "SNAKE"
-    word = word.upper
-    if checkWord(word):
-        print("Good")
 def get_random_word():
     wordlist = getWords(5)
     words = [
@@ -33,6 +27,31 @@ def get_random_word():
         if all(letter in ascii_letters for letter in word)
     ]
     return random.choice(words)
+
+def processWord(word,randword) ->bool:
+    word = word.upper()
+    if checkWord(word):
+        CorectLatters = []
+        MisplacedLatters = []
+        WrondLatters = []
+        ListWord=list(word)
+        ListRandword =list(randword)
+        for i in range(5):
+            if ListWord[i] == ListRandword[i] :
+                CorectLatters.append(ListWord[i])
+                ListRandword[i] = 0
+            elif (ListWord[i] in ListRandword) and ListWord[i] not in MisplacedLatters:
+                MisplacedLatters.append(ListWord[i])
+            elif ListWord[i] not in ListRandword:
+                WrondLatters.append(ListWord[i])
+        print(f"Corect Letters: {CorectLatters}")
+        print(f"Misplaced Letters: {MisplacedLatters}")
+        print(f"Wrong Letters: {WrondLatters}")
+        return True
+
+
+    else:
+        return False
 
 
 def game_over(word):
--- main.py
+++ main.py
@@ -1,9 +1,18 @@
-
-from functions import getWords ,processWord
-
-processWord("Value")
-from functions import get_random_word
+from functions import processWord, get_random_word,game_over
 
 word = get_random_word()
-print(word)
 
+ok = False
+for guess_num in range(1, 7):
+        guess = input(f"\nGuess {guess_num}: ")
+        while True:
+            if processWord(guess,word):
+                break
+            else:
+                guess = input(f"\nGuess {guess_num}: ")
+        if guess == word:
+            print("GOOD JOB!\n")
+            ok = True
+            break
+if not ok:
+    game_over(word)
